31ae4832d36284da33d5be65b06fe0e3d277ff4f
faisal <fsl4faisal@gmail.com>
2023-10-11 20:20:04 +0530

    first commit

---
+++ src/main/java/com/sharing/example/Sharing/Demo/SharingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sharing.example.Sharing.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharingDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharing/example/Sharing/Demo/SharingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharing.example.Sharing.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
